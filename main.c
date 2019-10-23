1ca068fed980a97e218a6d73d25127b2f7699178
Daniel Kucera <github@danman.eu>
2019-10-23 10:29:04 +0200

    big blocks

--- main.c
+++ main.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define BLOCK (1024*1024)
 #define _FILE_OFFSET_BITS 64
 
-int main() {
+int main(int argc, char *argv[]) {
 
    int fd;
-   unsigned char buff[BLOCK];
    int i;
    int rb;
    long pos = 0;
    int blk;
+   long blksize = 1024*1024*atoi(argv[1]);
+   unsigned char *buff = malloc(blksize);
 
    fd = open("/dev/sda", O_RDONLY);
 
    do {
      if (!(blk % 100))
-	printf("\n%08d: ", blk);
-     rb = read(fd, buff, BLOCK);
+	printf("\n%08dMB: ", pos/(1024*1024));
+     rb = read(fd, buff, blksize);
      //printf("pos %08d \n", pos);
      pos = rb + pos;
-     blk = pos/BLOCK;
+     blk = pos/blksize;
 //     printf("read %d bytes, pos %d                 \r", rb, pos/BLOCK);
 
-     for (i=0; i<BLOCK; i++){
+     for (i=0; i<blksize; i++){
   	   if (buff[i] != '\0'){
 		   //printf("not empty block at %d\n", i );
   		   break;
   	   }
      }
-     if (i==BLOCK) {
+     if (i==blksize) {
      	//printf("empty block at %x", ftell(fd) );
 //	printf("empty block at %d                   \n", pos/BLOCK );
 	printf("_");
      } else {
 	printf("X");
      }
-   } while (rb == BLOCK);
+   } while (rb == blksize);
 
    close(fd);
+
+   printf("\nfinished at pos %ld\n", pos);
 
 }
